1986b4a7c42401c8ac52d19c10eff9dc417f698f
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-07-20 09:45:28 +0700

    first commit

---
+++ FoxVoyage/App/FoxVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoxVoyageApp.swift
+//  FoxVoyage
+//
+//  Created by Nindya Alita Rosalia on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FoxVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
